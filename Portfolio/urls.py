12d1523b8612f4607fdceda81264d8d053cac04c
maazkhan911 <khanmaaz9111@mail.com>
2025-07-30 09:36:27 +0500

    Initial commit

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path
 from Portfolio import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , views.index , name='home')
